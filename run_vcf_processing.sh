cdda6a79dc1145722ca34f59c69c4c5a395c4ac6
ssaglam2 <sinansaglam6@gmail.com>
2025-05-27 16:41:53 +0200

    Update run_vcf_processing.sh

--- run_vcf_processing.sh
+++ run_vcf_processing.sh
@@ -53,7 +53,7 @@ OUTPUT_BASENAME="cohort_pipeline_merged"
 
 # 6. Chromosomes to keep.
 #    Space-separated list. IMPORTANT: Must match chromosome naming in your VCFs (e.g., "1" or "chr1").
-CHROMS_TO_KEEP="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22"
+CHROMS_TO_KEEP="chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22"
 # ====================================================================================
 # --- SCRIPT LOGIC --- (Generally, no need to modify below this line)
 # ====================================================================================
